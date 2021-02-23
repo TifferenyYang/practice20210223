85b08fbf06e79af76d8d95ba3918736bc50e4c0b
765820436@qq.com <765820436@qq.com>
2021-02-23 22:40:49 +0800

    testgit

---
+++ src/main/java/Test.java
@@ -0,0 +1,19 @@
+/**
+ * @program: JavaBase
+ * @Date: 2020/3/24 7:03
+ * @Author: lin.yang
+ * @Description:
+ */
+public class Test {
+ public static void main(String args[]) {
+  int result = 0;
+//  long f = 1;
+  for (int i = 1; i <= 100; i++) {
+   if (i % 2 == 0) {
+    result = result + i;
+   }
+
+  }
+  System.out.println("result = "+result);
+ }
+}
